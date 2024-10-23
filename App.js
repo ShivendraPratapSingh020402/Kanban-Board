a3951adebff12bbd6e2b12304abf256abd92339a
Shivendra <shivendrapratap.singh020402@gmail.com>
2024-10-23 10:07:18 +0530

    My second commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import KanbanBoard from './components/KanbanBoard';
+
 function App() {
   const [tickets, setTickets] = useState([]);
   const [groupBy, setGroupBy] = useState("status");
